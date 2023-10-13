36d563bd2d910960861706f0faf59de15127c973
Sonualam <sonualam1599@gmail.com>
2023-10-13 19:23:23 +0530

    dashboard done

--- inventory-management/src/pages/Dashboard.jsx
+++ inventory-management/src/pages/Dashboard.jsx
@@ -1,8 +1,22 @@
+import { useSelector } from "react-redux";
+import "../Css/Dashboard.css";
+
 export const Dashboard = () => {
+  const inventoryList = useSelector(
+    (state) => state.inventoryState.inventoryItems
+  );
+  const salesList = useSelector((state) => state.saleState.saleItems);
   return (
     <>
-      <div>
-        <div></div>
+      <div className="dashboardParent">
+        <div className="dashboardContainer">
+          Total: {inventoryList.length}
+          <p>Inventory</p>
+        </div>
+        <div className="dashboardContainer">
+          Total: {salesList.length}
+          <p>Sales</p>
+        </div>
       </div>
     </>
   );
